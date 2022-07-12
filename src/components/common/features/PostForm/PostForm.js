2f3dcbd116dffd44d1c79e994012561c1ed80f5a
Kamil <kaz4486@gmail.com>
2022-07-12 10:28:23 +0200

    Add Form validation

--- src/components/common/features/PostForm/PostForm.js
+++ src/components/common/features/PostForm/PostForm.js
@@ -6,8 +6,18 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useForm } from 'react-hook-form';
 
 const PostForm = ({ action, actionText, ...props }) => {
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
+
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
+
   const [title, setTitle] = useState(props.title || '');
   const [author, setAuthor] = useState(props.author || '');
 
@@ -18,33 +28,54 @@ const PostForm = ({ action, actionText, ...props }) => {
   const [content, setMainContent] = useState(props.content || '');
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    action({ title, author, publishedDate, shortDescription, content });
+    setContentError(!content);
+    setDateError(!publishedDate);
+    if (content && publishedDate) {
+      action({ title, author, publishedDate, shortDescription, content });
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={validate(handleSubmit)}>
       <Form.Label>Title</Form.Label>
       <Form.Control
+        {...register('title', { required: true, minLength: 4 })}
         type='text'
         placeholder='Enter title'
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
+      {errors.title && (
+        <small className='d-block form-text text-danger mt-2'>
+          This field is required, minimum 4 signs
+        </small>
+      )}
       <Form.Label>Author</Form.Label>
       <Form.Control
+        {...register('author', { required: true, minLength: 4 })}
         type='text'
         placeholder='Enter author'
         value={author}
         onChange={(e) => setAuthor(e.target.value)}
       />
+      {errors.author && (
+        <small className='d-block form-text text-danger mt-2'>
+          This field is required, minimum 4 signs
+        </small>
+      )}
       <Form.Label>Published</Form.Label>
       <DatePicker
         selected={publishedDate}
         onChange={(date) => setPublished(date)}
       />
+      {dateError && (
+        <small className='d-block form-text text-danger mt-2'>
+          This field is required
+        </small>
+      )}
       <Form.Label>Short description</Form.Label>
       <Form.Control
+        {...register('shortDescription', { required: true, minLength: 20 })}
         type='text'
         placeholder='Leave a comment here'
         as='textarea'
@@ -52,15 +83,25 @@ const PostForm = ({ action, actionText, ...props }) => {
         value={shortDescription}
         onChange={(e) => setShortDescription(e.target.value)}
       />
+      {errors.shortDescription && (
+        <small className='d-block form-text text-danger mt-2'>
+          This field is required, minimum 20 signs
+        </small>
+      )}
       <Form.Label>Main content</Form.Label>
       <ReactQuill
         type='text'
         theme='snow'
         placeholder='Leave a comment here'
         className='my-editing-area'
-        onChange={setMainContent}
         value={content}
+        onChange={setMainContent}
       />
+      {contentError && (
+        <small className='d-block form-text text-danger mt-2'>
+          This field is required, minimum 4 signs
+        </small>
+      )}
       <Button variant='primary' type='submit' className='mt-2'>
         {actionText}
       </Button>
--- src/utils/dateToStr.js
+++ src/utils/dateToStr.js
@@ -2,6 +2,6 @@ export const dateToStr = (publishedDate) => {
   const dd = String(publishedDate.getDate()).padStart(2, '0');
   const mm = String(publishedDate.getMonth() + 1).padStart(2, '0'); //January is 0!
   const yyyy = publishedDate.getFullYear();
-  console.log(mm + '/' + dd + '/' + yyyy);
+
   return mm + '/' + dd + '/' + yyyy;
 };
