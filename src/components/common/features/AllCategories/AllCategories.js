34f2cbaa6d4b5c61b754bd65fc0839b364bc49fa
Kamil <kaz4486@gmail.com>
2022-07-13 13:37:52 +0200

    Add styles for AllCategories

--- src/components/common/features/AllCategories/AllCategories.js
+++ src/components/common/features/AllCategories/AllCategories.js
@@ -1,4 +1,4 @@
-import { Card, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getAllCategories } from '../../../../redux/categoriesRedux';
@@ -8,18 +8,27 @@ const AllCategories = () => {
   const categories = useSelector((state) => getAllCategories(state));
 
   return (
-    <Container className='p-0'>
-      <section className={clsx('justify-content-between')}>
-        {categories.map((category) => (
-          <Row key={category.id}>
-            <Card key={category.id}>
-              <Link key={category.id} to={'/category/' + category.name}>
-                <p>{category.name}</p>
-              </Link>
-            </Card>
-          </Row>
-        ))}
-      </section>
+    <Container className='mt-3'>
+      <Row>
+        <Col
+          align='left'
+          xs={1}
+          md={6}
+          lg={8}
+          className={clsx('justify-content-between')}
+        >
+          {categories.map((category) => (
+            <Row key={category.id}>
+              <Card key={category.id}>
+                <Link key={category.id} to={'/category/' + category.name}>
+                  <p>{category.name}</p>
+                </Link>
+              </Card>
+            </Row>
+          ))}
+        </Col>
+        <Col xs={1} md={3} lg={4}></Col>
+      </Row>
     </Container>
   );
 };
--- src/components/common/features/views/pages/Categories/Categories.js
+++ src/components/common/features/views/pages/Categories/Categories.js
@@ -1,13 +1,12 @@
 import { Container, Row, Col } from 'react-bootstrap';
-
 import AllCategories from '../../../AllCategories/AllCategories';
 
 const Categories = () => {
   return (
     <Container>
-      <Col>
-        <Row>
-          <h2>All categories</h2>
+      <Col align='left' xs={1} md={6} lg={8} className='m-auto '>
+        <Row className='pt-4 pb-2'>
+          <h2 align='left'>All categories</h2>
         </Row>
         <AllCategories />
       </Col>
